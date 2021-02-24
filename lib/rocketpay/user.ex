1d14e295c54af534aa7f9e8ff4cc2f0e47d07a12
Bruno Pedrosa <bgpedrosas@gmail.com>
2021-02-24 16:30:50 -0300

    create changeset for user schema

--- lib/rocketpay/user.ex
+++ lib/rocketpay/user.ex
@@ -4,11 +4,19 @@ defmodule Rocketpay.User do
 
   @primary_key {:id, :binary_id, autogenerate: true}
 
+  @required_params [:name, :age, :email, :password_hash, :nickname]
+
   schema "users" do
     field :name, :string
     field :age, :integer
     field :email, :string
     field :password_hash, :string
     field :nickname, :string
+  end
+
+  def changeset(params) do
+    %__MODULE__{}
+    |> cast(params, @required_params)
+    |> validate_required(@required_params)
   end
 end
